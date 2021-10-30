87a7c8f58fdcf4fae74cf22b89873b1c5db3879a
Muhammad Irtiza <91867702+Irtiza751@users.noreply.github.com>
2021-10-31 00:37:05 +0500

    Update index.js

--- src/styles/GlobalComponents/index.js
+++ src/styles/GlobalComponents/index.js
@@ -1,4 +1,6 @@
 import styled from 'styled-components'
+// padding: ${(props) => props.nopadding ? "0" : "16px 16px 0" };
+
 export const Section = styled.section`
   display: ${(props) => props.grid ? "grid" : "flex" };
   flex-direction: ${(props) => props.row ? "row" : "column" };
@@ -16,7 +18,7 @@ export const Section = styled.section`
   }
 
   @media ${(props) => props.theme.breakpoints.sm} {
-    padding: ${(props) => props.nopadding ? "0" : "16px 16px 0" } ;
+    padding: 0;
     width: calc(100vw - 32px);
     flex-direction: column;
   }
